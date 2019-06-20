755f589f20f11a34fb8fc8c63c79c2ab51b225f0
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-06-19 19:05:04 -0500

    Invoking function using New Operator

---
+++ new_function.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**USed to receive value from the server dynamically */
+// Invoke using new Function by passing string arguments
+let foo = new Function("a", "b", "return a + b");
+
+console.log(`Invoke new Function -> ${foo(10, 20)}`);
+
+let sayHi = new Function('console.log("::Hello String Param::")');
+sayHi(); // Hello
+
+function looper() {
+  for (let index = 0; index < 23; index++) {
+    console.log(index);
+  }
+}
+console.log();
+
+console.log("Call Looper....");
+let bar = new Function(
+  "x",
+  "for (let index = 1; index < x; index++) {console.log(index);}"
+);
+bar(20);
