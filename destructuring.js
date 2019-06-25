f61e6ff29701576fad3fbf63db2ab4eb4743806a
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-06-25 02:43:49 -0500

    Determine top salaries from a list of objects

--- destructuring.js
+++ destructuring.js
@@ -70,3 +70,54 @@ let {
 console.log(`Size props::-> `, s.height, s.width);
 console.log(`Items props::-> `, item1, item2);
 console.log(`Extra props::-> `, x);
+
+// Write the destructuring assignment that reads:
+
+//     name property into the variable name.
+//     years property into the variable age.
+//     isAdmin property into the variable isAdmin (false, if no such property)
+
+console.log();
+user = {
+  name: "John",
+  years: 30
+};
+
+let { name, years: age, isAdmin = false } = user;
+console.log(`Name `, name);
+console.log(`Name `, age);
+console.log(`Name `, isAdmin);
+
+// Create the function topSalary(salaries) that returns the name of the top-paid person.
+
+//     If salaries is empty, it should return null.
+//     If there are multiple top-paid persons, return any of them.
+
+// P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+console.log();
+console.log();
+
+let salaries = {
+  John: 100,
+  Peter: 300,
+  Mary: 250
+};
+
+console.log(`Highest Salary in `, salaries);
+
+let topSalary = salaries => {
+  salaries === "" && null; // Return null if salaries is empty
+  let highest = 0,
+    userName = "";
+
+  for (let [name, value] of Object.entries(salaries)) {
+    if (value > highest) {
+      highest = value;
+      userName = name;
+    }
+  }
+
+  return userName;
+};
+
+console.log(`Highest Salary-> ${topSalary(salaries)}`);
