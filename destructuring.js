5285ce7554b8f83ee5cdb69cfd8198e8fbc595fc
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-06-25 00:28:50 -0500

    Nested Destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,19 +1,24 @@
-let arr = ["Ilya", "Kantor"];
+"use strict";
+// Destructure into first name and surname
+console.log(`Array Destructure`);
+let arr = ["Eugene", "Kantor"];
 
-[firstName, lastName] = arr;
-console.log(`Hello ${firstName} ${lastName}`);
+let [first, last] = arr;
+console.log(`First name:-> ${first} Last name:-> ${last}`);
 
-var [firstName, , position] = [
+console.log();
+
+var [firstName, , ...position] = [
   "Julius",
   "Caesar",
   "Consul",
   "of the Roman Republic"
 ];
 
-console.log(`Name ${firstName} title ${position}`);
+console.log(`Name:: ${firstName} Title:: ${position}`);
 
 console.log();
-console.log(`Object ......`);
+console.log(`Object Destructure`);
 
 // Assign to an object property
 let user = {};
@@ -27,11 +32,29 @@ let options = {
 };
 // Destructure an object
 let { title, width, height } = options;
-console.log(`${title} ${height} ${width}`);
+console.log(`Title:: ${title} Height:: ${height} Width:: ${width}`);
 
 console.log(".................");
 console.log();
-let options = {
+console.log(`Destructure with the spread operator`);
+
+options = {
+  title: "Menu",
+  height: 200,
+  width: 100
+};
+
+// title = property named title
+// rest = object with the rest of properties
+let { title: t, ...rest } = options;
+
+// now title="Menu", rest={height: 200, width: 100}
+console.log(`Rest operator height `, rest.height); // 200
+console.log(`Rest operator width `, rest.width); // 100
+
+console.log();
+console.log(`Nested Destructuring...`);
+options = {
   size: {
     width: 100,
     height: 200
@@ -39,3 +62,11 @@ let options = {
   items: ["Cake", "Donut"],
   extra: true // something extra that we will not destruct
 };
+let {
+  size: s,
+  extra: x,
+  items: [item1, item2]
+} = options;
+console.log(`Size props::-> `, s.height, s.width);
+console.log(`Items props::-> `, item1, item2);
+console.log(`Extra props::-> `, x);
